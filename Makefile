c79b51a2bce1c4690ad24091a86a3c6ed6ed9836
Samuel Breese <samuel@chame.co>
2016-06-07 16:15:01 -0400

    Anonymization

--- Makefile
+++ Makefile
@@ -8,7 +8,7 @@ LEXERS = lexer/c/lex lexer/python/lex lexer/java/lex
 
 CFLAGS_gcc = -Iinclude -std=c99 -O2 -g -Wall -Werror -D_POSIX_C_SOURCE=200809 -Wno-unused-result
 CFLAGS = $(CFLAGS_$(CC))
-LINKER_FLAGS_gcc = -lm
+LINKER_FLAGS_gcc = -lm -lpcre
 LINKER_FLAGS = $(LINKER_FLAGS_$(CC))
 
 vpath %.c src
@@ -25,7 +25,7 @@ $(BUILD_DIR):
 $(BUILD_DIR)/%: %.c
 	$(CC) -o $@ $(CFLAGS) $(LINKER_FLAGS) $<
 
-lexer/%/lex: lexer/%/lex.l lexer/%/symtab.h
+lexer/%/lex: lexer/%/lex.l lexer/%/tokens.h
 	flex -o $@.out.c $@.l
 	$(CC) $@.out.c -o $@ -lfl
 
---
+++ src/anonymize.c
@@ -0,0 +1,113 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <stddef.h>
+#include <stdbool.h>
+#include <string.h>
+
+#include <pcre.h>
+
+#include "config.h"
+#include "utils.h"
+
+typedef struct name_entry {
+	char name[1024];
+	char new[1024];
+	struct name_entry *next;
+} name_entry;
+
+name_entry *NAMES = NULL;
+
+char *RIN_PATTERN = "(66[0-9]{7})";
+
+void add_name(char *name, char *new)
+{
+	name_entry *n = (name_entry *) malloc(sizeof(name_entry));
+	strncpy(n->name, name, 1024);
+	strncpy(n->new, new, 1024);
+	n->next = NAMES;
+	NAMES = n;
+}
+
+unsigned int hash(char *key)
+{
+	unsigned int h = 5381;
+	for (unsigned int i = 0; i < strlen(key); ++i) {
+		h = h * 33 + key[i];
+	}
+	return abs(h % FINGERPRINT_CACHE_SIZE);
+}
+
+void scramble_name(char *name, char *new)
+{
+	snprintf(new, 1024, "REDACTED_NAME_%3u", hash(name));
+}
+
+void apply_replace(char *buf, char *str, name_entry *entry)
+{
+	memset(buf, 0, 1024);
+	int len = strlen(str);
+	int name_len = strlen(entry->name);
+	int new_len = strlen(entry->new);
+	for (int i = 0; i < len; ++i) {
+		if (strncmp(str + i, entry->name, name_len) == 0) {
+			strncat(buf, entry->new, new_len);
+			i += name_len - 1;
+		} else {
+			*(strchr(buf, 0)) = str[i];
+		}
+	}
+}
+
+int main(int argc, char **argv)
+{
+	bool read_1 = false, read_2 = false;
+	FILE *name_file, *new_file;
+	if (argc >= 2) {
+		read_1 = true;
+		name_file = fopen(argv[1], "r");
+	}
+	if (argc >= 3) {
+		read_2 = true;
+		new_file = fopen(argv[2], "r");
+	}
+	if (read_1) {
+		char name[1024], new[1024];
+		while (fscanf(name_file, " %s ", name) == 1) {
+			if (read_2) fscanf(new_file, " %s ", new);
+			else scramble_name(name, new);
+			add_name(name, new);
+		}
+	}
+
+	char line[4096];
+	const char *error;
+	int erroroffset;
+	pcre *rin_re = pcre_compile(
+			RIN_PATTERN,
+			0,
+			&error,
+			&erroroffset,
+			NULL);
+	if (rin_re == NULL)
+	{
+		fprintf(stderr, "PCRE2 compilation failed at offset %d: %s\n", erroroffset, error);
+		return 1;
+	}
+	int ovector[30];
+	while (fgets(line, 4096, stdin) != NULL) {
+		char buf[4096];
+		for (name_entry *n = NAMES; n != NULL; n = n->next) {
+			apply_replace(buf, line, n);
+			memcpy(line, buf, 4096);
+		}
+		int rc;
+		while ((rc = pcre_exec(rin_re, NULL, line, strlen(line), 0, 0, ovector, 30)) > 0) {
+			char *start = line + ovector[0];
+			char buffer[10] = {0};
+			memcpy(buffer, start, 9);
+			snprintf(buffer, 10, "%09d", atoi(buffer) % 523);
+			memcpy(start, buffer, 9);
+		}
+		printf("%s", line);
+	}
+}
