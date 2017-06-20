ef97980d994204113f9c89074ef72ec2c09b5d10
Samuel Breese <samuel@chame.co>
2017-06-20 13:58:15 -0400

    Commit Python lexer

--- src/Lichen/Config/Languages.hs
+++ src/Lichen/Config/Languages.hs
@@ -3,6 +3,10 @@ module Lichen.Config.Languages where
 import Lichen.Config
 
 import qualified Lichen.Lexer.C as C
+import qualified Lichen.Lexer.Python as Python
 
-langC = Language ["c", "h", "cpp", "hpp", "C", "H", "cc"] C.lex (WinnowConfig 9 5)
+langC = Language [".c", ".h", ".cpp", ".hpp", ".C", ".H", ".cc"] C.lex (WinnowConfig 9 5)
 configC = defaultPlagiarismConfig { language = langC }
+
+langPython = Language [".py"] Python.lex (WinnowConfig 9 5)
+configPython = defaultPlagiarismConfig { language = langPython }
