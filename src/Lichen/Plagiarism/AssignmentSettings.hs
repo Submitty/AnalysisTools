7701714756279d781f06be05104fa9eb352e9727
Samuel Breese <samuel@chame.co>
2017-06-26 10:12:06 -0400

    Temporarily removing aeson depencency for build time

--- src/Lichen/Plagiarism/AssignmentSettings.hs
+++ src/Lichen/Plagiarism/AssignmentSettings.hs
@@ -2,22 +2,9 @@
 
 module Lichen.Plagiarism.AssignmentSettings where
 
-import Data.Aeson
-import qualified Data.Text as T
-import qualified Data.ByteString.Lazy as BS
-
-import Control.Monad.Except
-
 import Lichen.Config.Plagiarism
-import Lichen.Error
 
 newtype AssignmentSettings = AssignmentSettings { activeVersion :: Int }
-instance FromJSON AssignmentSettings where
-        parseJSON = withObject "assignment_settings"
-                    $ \o -> AssignmentSettings <$> o .: "active_version"
 
 getAssignmentSettings :: FilePath -> Plagiarism AssignmentSettings
-getAssignmentSettings p = do
-        file <- liftIO $ BS.readFile p
-        case eitherDecode file of Left e -> throwError . JSONDecodingError $ T.pack e
-                                  Right t -> return t
+getAssignmentSettings = undefined
