5d873f19f086ec6f1d289f206a9a0d8a409394a2
Samuel Breese <samuel@chame.co>
2017-06-28 17:01:53 -0400

    Fix typo

--- build.sh
+++ build.sh
@@ -14,7 +14,7 @@ then stack --allow-different-user --install-ghc --copy-bins build
 else for b in $BINARIES;
         do if [ -e $b ]
             then echo "File exists, no need to pull release"
-            else wget "https://github.com/Submitty/AnalysisTools/releases/download/$1/$VERSION"
+            else wget "https://github.com/Submitty/AnalysisTools/releases/download/$VERSION/$b"
         fi;
     done
 fi
