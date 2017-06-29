6e1d6ee635236fc14d318cf6b1405bfd0b17dbad
Samuel Breese <samuel@chame.co>
2017-06-29 10:46:40 -0400

    Add jquery to plagiarism report pages

--- src/Lichen/Plagiarism/Render.hs
+++ src/Lichen/Plagiarism/Render.hs
@@ -14,11 +14,15 @@ hs = H.toHtml . show
 renderPage :: Config -> H.Html -> H.Html
 renderPage config b = H.docTypeHtml $ mconcat
     [ H.head $ mconcat
-        [ H.title . H.toHtml $ reportTitle config
+        [ H.meta ! A.charset "utf-8"
+        , H.meta ! A.httpEquiv "X-UA-Compatible" ! A.content "IE=edge"
+        , H.meta ! A.name "viewport" ! A.content "width=device-width, initial-scale=1"
+        , H.title . H.toHtml $ reportTitle config
         , H.link ! A.rel "stylesheet" ! A.href "https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css"
         ]
     , H.body $ mconcat
         [ b
+        , H.script ! A.src "https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js" $ ""
         , H.script ! A.src "https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" $ ""
         ]
     ]
