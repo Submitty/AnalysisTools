08a0fbbadb2306064ec42cabafede31341962741
Samuel Breese <samuel@chame.co>
2017-06-30 13:32:17 -0400

    Scrollbar fix

--- src/Lichen/Plagiarism/Render/Compare.hs
+++ src/Lichen/Plagiarism/Render/Compare.hs
@@ -13,11 +13,11 @@ renderCompare :: Show a => (Double, (Fingerprints, a), (Fingerprints, a)) -> H.H
 renderCompare (m, (_, t), (_, t')) = H.div ! A.class_ "container" $ mconcat
     [ H.h1 ! A.class_ "centered" $ H.toHtml (sq t ++ " vs. " ++ sq t' ++ ": " ++ sq (m * 100) ++ "% Match")
     , H.div ! A.class_ "row" $ mconcat
-        [ H.div ! A.class_ "col-sm-6"
+        [ H.div ! A.class_ "col-sm-6 hide-scroll"
             $ H.div ! A.id "left" ! A.class_ "scrollable-pane" $ mconcat
                 [ H.span ! A.id "112324124345543757457586456" ! A.class_ "highlight green" $ "hello"
                 ]
-        , H.div ! A.class_ "col-sm-6"
+        , H.div ! A.class_ "col-sm-6 hide-scroll"
             $ H.div ! A.id "right" ! A.class_ "scrollable-pane" $ mconcat
                 [ "hello"
                 ]
