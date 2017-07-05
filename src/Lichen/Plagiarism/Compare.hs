4838cb50d1ced79414a94e25b4ccee1e881216be
Samuel Breese <samuel@chame.co>
2017-07-05 11:33:57 -0400

    Comparison highlights

--- src/Lichen/Plagiarism/Compare.hs
+++ src/Lichen/Plagiarism/Compare.hs
@@ -6,18 +6,20 @@ import qualified Data.Set as Set
 import Lichen.Plagiarism.Winnow
 
 -- Naively compare two sets of fingerprints to obtain a percent match.
-compareFingerprints :: Fingerprints -> Fingerprints -> Double
-compareFingerprints al bl = fromIntegral (Set.size (Set.intersection a b)) / fromIntegral (Set.size (Set.union a b)) where
-    a = Set.fromList $ fst <$> al
-    b = Set.fromList $ fst <$> bl
+compareFingerprints :: ((Fingerprints, a), (Fingerprints, a)) -> (Double, (Fingerprints, a), (Fingerprints, a))
+compareFingerprints ((al, x), (bl, y)) = (fromIntegral (Set.size un) / fromIntegral (Set.size is), (Set.toList is, x), (Set.toList is, y)) where
+    a = Set.fromList al
+    b = Set.fromList bl
+    is = Set.intersection a b
+    un = Set.union a b
 
 -- Given a list of pairs of fingerprints and an associated tag (typically
 -- the source file from which those fingeprints were generated), compare
 -- each possible pair of fingerprints, returning a list of percent matches
 -- associated with the tags of the two fingeprint sets compared.
 crossCompare :: [(Fingerprints, a)] -> [(Double, (Fingerprints, a), (Fingerprints, a))]
-crossCompare prints = map cmp $ pairs prints
-    where cmp (n1@(f1, _), n2@(f2, _)) = (compareFingerprints f1 f2, n1, n2)
+crossCompare prints = compareFingerprints <$> pairs prints
+    where pairs :: [(Fingerprints, a)] -> [((Fingerprints, a), (Fingerprints, a))]
           pairs lst = tails lst >>= subpairs
           subpairs [] = []
           subpairs (x:xs) = (\y -> (x, y)) <$> xs
