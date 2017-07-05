b2c14afaaa639e6f60ced3426dac7aff645af7ba
Samuel Breese <samuel@chame.co>
2017-07-05 12:00:34 -0400

    Fix comparison

--- src/Lichen/Plagiarism/Compare.hs
+++ src/Lichen/Plagiarism/Compare.hs
@@ -3,13 +3,16 @@ module Lichen.Plagiarism.Compare where
 import Data.List
 import qualified Data.Set as Set
 
+import Lichen.Lexer
 import Lichen.Plagiarism.Winnow
 
 -- Naively compare two sets of fingerprints to obtain a percent match.
 compareFingerprints :: ((Fingerprints, a), (Fingerprints, a)) -> (Double, (Fingerprints, a), (Fingerprints, a))
-compareFingerprints ((al, x), (bl, y)) = (fromIntegral (Set.size un) / fromIntegral (Set.size is), (Set.toList is, x), (Set.toList is, y)) where
-    a = Set.fromList al
-    b = Set.fromList bl
+compareFingerprints ((al, x), (bl, y)) = (fromIntegral (Set.size un) / fromIntegral (Set.size is), (matching is al, x), (matching is bl, y)) where
+    matching :: Set.Set Int -> Fingerprints -> Fingerprints
+    matching s = filter (flip Set.member s . tdata)
+    a = Set.fromList (tdata <$> al)
+    b = Set.fromList (tdata <$> bl)
     is = Set.intersection a b
     un = Set.union a b
 
