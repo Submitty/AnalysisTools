a66c6474c7a8254ac1526aa916ed5d385480f3bc
Samuel Breese <samuel@chame.co>
2017-07-10 13:05:05 -0400

    Links on index page

--- src/Lichen/Plagiarism/Render/Index.hs
+++ src/Lichen/Plagiarism/Render/Index.hs
@@ -5,12 +5,15 @@ module Lichen.Plagiarism.Render.Index where
 import Data.Foldable
 import Data.Monoid ((<>))
 
+import Text.Blaze.Html5 ((!))
 import qualified Text.Blaze.Html5 as H
+import qualified Text.Blaze.Html5.Attributes as A
 
+import Lichen.Util
 import Lichen.Plagiarism.Render
 
 renderEntry :: Show a => (Double, (b, a), (b, a)) -> H.Html
-renderEntry (match, (_, x), (_, y)) = H.tr (H.td (hs match) <> H.td (hs x) <> H.td (hs y))
+renderEntry (match, (_, x), (_, y)) = H.tr (H.td (H.a ! A.href (H.stringValue $ "compare/" ++ sq x ++ "_" ++ sq y ++ ".html") $ hs match) <> H.td (hs x) <> H.td (hs y))
 
 renderTable :: Show a => [(Double, (b, a), (b, a))] -> H.Html
 renderTable t = H.table (header <> traverse_ renderEntry t) where
