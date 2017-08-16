7274f26a7107d67e9518484f5dedf3521b0ac766
Samuel Breese <samuel@chame.co>
2017-08-16 14:19:56 -0400

    Update diagnostics, changes to build

--- src/Lichen/Config/Diagnostics.hs
+++ src/Lichen/Config/Diagnostics.hs
@@ -11,18 +11,24 @@ import Lichen.Config.Languages
 data Config = Config
             { dataDir :: FilePath
             , language :: Language
+            , mode :: String
+            , tag :: Maybe String
             , sourceFiles :: [FilePath]
             }
 instance FromJSON Config where
         parseJSON = withObject "config_diagnostics" $ \o -> do
             dataDir <- fromMaybe (dataDir defaultConfig) <$> o .:? "data_dir"
             language <- fromMaybe (language defaultConfig) <$> o .:? "language"
+            mode <- fromMaybe (mode defaultConfig) <$> o .:? "mode"
+            tag <- fromMaybe (tag defaultConfig) <$> o .:? "tag"
             sourceFiles <- fromMaybe (sourceFiles defaultConfig) <$> o .:? "source_files"
             return Config{..}
 
 defaultConfig :: Config
 defaultConfig = Config { dataDir = ".lichen"
                        , language = langDummy
+                       , mode = ""
+                       , tag = Nothing
                        , sourceFiles = []
                        }
 
