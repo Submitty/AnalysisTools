1de5f445543b5fd592f52b30366faa8c57514106
Elizabeth Dinella <elizabeth.a.dinella@gmail.com>
2018-01-04 18:33:09 -0500

    fixed syntax errors

--- commonAST/compareTest.py
+++ commonAST/compareTest.py
@@ -4,7 +4,7 @@ import glob
 
 #run sumbitty count
 
-testLang(lang):
+def testLang(lang):
 	directory = "/usr/local/submitty/GIT_CHECKOUT_AnalysisTools/commonAST/py-test-files/"
 	if(lang == "python"):
 		directory += "*.py"	
@@ -12,7 +12,7 @@ testLang(lang):
 		directory += "*.cpp"
 
 	for fname in glob.glob(directory):
-		subprocess.call(["/usr/local/submitty/SubmittyAnalysisTools/count", "-l", lang, whatToCount, fname)
+		subprocess.call(["/usr/local/submitty/SubmittyAnalysisTools/count", "-l", lang, whatToCount, fname])
  
 
  #node -l python for /var/local/submitty/courses/f17/tutorial/submissions/for_test_cpp/instructor/13/part1/*.py
