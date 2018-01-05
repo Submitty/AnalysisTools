afb29e6ad0db613f2b9db6e4d6d3c28ef27c4bc7
Elizabeth Dinella <elizabeth.a.dinella@gmail.com>
2018-01-04 21:41:52 -0500

    fixed output of commonast and submitty_count calls

--- commonAST/compareTest.py
+++ commonAST/compareTest.py
@@ -4,7 +4,7 @@ import glob
 
 #run sumbitty count
 
-def testLang(lang, whatToCount):
+def testLang(lang, whatToCount, arg):
 	directory = "/usr/local/submitty/GIT_CHECKOUT_AnalysisTools/commonAST/py-test-files/"
 	if(lang == "python"):
 		directory += "*.py"	
@@ -15,16 +15,21 @@ def testLang(lang, whatToCount):
 
 	for fname in glob.glob(directory):
 		#print(fname)
-		submittyCount = subprocess.call(["/usr/local/submitty/SubmittyAnalysisTools/count", "node", "-l", lang, whatToCount, fname])
-		commonAST = subprocess.call(["/usr/local/submitty/SubmittyAnalysisTools/commonast.py", "-"+commonastlang, "-"+whatToCount.capitalize(), fname])
+		#submittyCount = subprocess.call(["/usr/local/submitty/SubmittyAnalysisTools/count", "node", "-l", lang, whatToCount, fname])
+
+		call = "/usr/local/submitty/SubmittyAnalysisTools/count node -l " + lang + " " + whatToCount + " " + fname
+		submittyCount = subprocess.Popen(["/bin/bash", "-i", "-c", call])
+		#commonAST = subprocess.call(["/usr/local/submitty/SubmittyAnalysisTools/commonast.py", "-"+commonastlang, "-"+whatToCount.capitalize(), fname, arg])
+		call = "/usr/local/submitty/SubmittyAnalysisTools/commonast.py " + "-"+commonastlang + " -"+whatToCount.capitalize() + " " + fname + " " + arg
+		commonAST = subprocess.Popen(["/bin/bash", "-i", "-c", call])
 
 		print("EQUAL?", submittyCount == commonAST)
 
  #node -l python for /var/local/submitty/courses/f17/tutorial/submissions/for_test_cpp/instructor/13/part1/*.py
 
 print("number of for loops")
-testLang("python", "for")
-testLang("c", "for")
+testLang("python", "for", "Void")
+testLang("c", "for", "Void")
 print("number of while loops")
-testLang("python", "while")
-testLang("c", "while")
+testLang("python", "while", "Void")
+testLang("c", "while", "Void")
