56ea1d6e3b44214b7310f1bd942833008fad83e0
Elizabeth Dinella <elizabeth.a.dinella@gmail.com>
2018-01-04 21:17:17 -0500

    Added calls to commonAST

--- commonAST/compareTest.py
+++ commonAST/compareTest.py
@@ -8,17 +8,23 @@ def testLang(lang, whatToCount):
 	directory = "/usr/local/submitty/GIT_CHECKOUT_AnalysisTools/commonAST/py-test-files/"
 	if(lang == "python"):
 		directory += "*.py"	
+		commonastlang = "py"
 	elif(lang == "c"):
 		directory += "*.cpp"
+		commonastlang = "cpp"
 
 	for fname in glob.glob(directory):
 		#print(fname)
-		subprocess.call(["/usr/local/submitty/SubmittyAnalysisTools/count", "node", "-l", lang, whatToCount, fname])
- 
+		submittyCount = subprocess.call(["/usr/local/submitty/SubmittyAnalysisTools/count", "node", "-l", lang, whatToCount, fname])
+		commonAST = subprocess.call(["/usr/local/submitty/SubmittyAnalysisTools/commonast.py", "-"+commonastlang, "-"+whatToCount.capitalize(), fname])
+
+		print("EQUAL?", submittyCount == commonAST)
 
  #node -l python for /var/local/submitty/courses/f17/tutorial/submissions/for_test_cpp/instructor/13/part1/*.py
 
+print("number of for loops")
 testLang("python", "for")
 testLang("c", "for")
+print("number of while loops")
 testLang("python", "while")
 testLang("c", "while")
