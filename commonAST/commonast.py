409ff2f2b71c5949077feccf9e3eb0bfa7b25b36
Elizabeth Dinella <elizabeth.a.dinella@gmail.com>
2018-02-13 08:42:45 -0500

    Switched to a count mode and a print mode and added functionality for multiple files

--- commonAST/commonast.py
+++ commonAST/commonast.py
@@ -3,48 +3,86 @@
 import subprocess
 import sys
 
+#function to make sure we don't get an array out of bounds error when getting args
+def checkNumArgs(numArgs):
+	if len(sys.argv) < numArgs:
+		print("ERROR!:", numArgs, "arguments required")
+		print("only provided", len(sys.argv))
+		sys.exit()	
+
 #To do:
 #	more than one file?
-#	fix python errors
-#	fix why its running python
-#	documentation?
 
-if len(sys.argv) < 5:
-	print("ERROR!: 5 arguments required")
-	print("only provided", len(sys.argv))
-	sys.exit()	
-
-lang = sys.argv[1]
-countType = sys.argv[2]
-countArg = sys.argv[3]
-filename = sys.argv[4]
-option = "" 
-
-if len(sys.argv) > 5:
-	option = sys.argv[5]
+#function to format json option and print it
+def printOption(option):
 	option = option[1:]
 	option = option.lower()
-'''
-if len(sys.argv) > 5:
-	count = 5
+	subprocess.call([commonASTtraversalPath, "out.txt", option])
+
+def getCommonASTinOut(lang, filename):
+	if lang == "-py":
+		subprocess.call(["python3", astMatcher_pythonPath, filename])
+	elif lang == "-cpp":
+		f = open("out.txt", "w")
+		subprocess.call([astMatcher_cppPath, filename], stdout=f)
+	else:
+		print ("invalid language")
+		sys.exit()
+
+#To do:
+#	more than one file?
+
+astMatcher_pythonPath = "/usr/local/submitty/SubmittyAnalysisTools/astMatcher.py"
+astMatcher_cppPath = "/usr/local/submitty/clang-llvm/build/bin/ASTMatcher"
+commonASTtraversalPath = "/usr/local/submitty/SubmittyAnalysisTools/commonASTCount.out"
+
+
+#if there are less than 2 arguments, exit
+checkNumArgs(2)
+
+#if we're in count mode, lang is the first argument
+#if we're printing json, that will be the first argument
+langOrOption = sys.argv[1]
+
+filenames = []
+filename = None
+lang = None
+countArg = None
+countType = None
+minNumArgs = None
+
+#find out if we're counting nodes or just printing json
+countMode = False
+if(langOrOption == "-py" or langOrOption == "-cpp"):
+	countMode = True	
+	minNumArgs = 5
+	checkNumArgs(minNumArgs)
+	#we're in count mode so the arguments are in this order
+	lang = langOrOption
+	countType = sys.argv[2]
+	countArg = sys.argv[3]
+	filename = sys.argv[4]
+else:
+	#we're in print mode so the arguments are in this order 
+	minNumArgs = 4	
+	checkNumArgs(minNumArgs)
+	filename = sys.argv[2]
+	lang = sys.argv[3]
+
+filenames.append(filename)
+
+if len(sys.argv) > minNumArgs:
+	count = minNumArgs
 	while(count < len(sys.argv)):
 		filenames.append(sys.argv[count])
 		count+=1
-'''
 
-if lang == "-py":
-	#for fname in filenames:
-		subprocess.call(["python3", "/usr/local/submitty/SubmittyAnalysisTools/astMatcher.py", filename])
-	
-elif lang == "-cpp":
-	#for fname in filenames:
-		f = open("out.txt", "w")
-		alias = "/usr/local/submitty/clang-llvm/build/bin/ASTMatcher"
-		subprocess.call([alias, filename], stdout=f)
-else:
-	print ("invalid language")
-	sys.exit()
-
-subprocess.call(["/usr/local/submitty/SubmittyAnalysisTools/commonASTCount.out", "out.txt", countType, countArg, option])
-
-
+total = 0
+for fname in filenames:
+	getCommonASTinOut(lang, fname)
+	if countMode:
+		total += int(subprocess.check_output([commonASTtraversalPath, "out.txt", countType, countArg]))
+	else:
+		printOption(langOrOption)
+if(countMode):
+	print(total)
