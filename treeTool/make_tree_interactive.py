32fdb9914b50ba30185b98a117315fd0acb9049b
root <root@vagrant.vm>
2018-04-17 19:56:22 -0400

    changed shade of matched blue

--- treeTool/make_tree_interactive.py
+++ treeTool/make_tree_interactive.py
@@ -1,10 +1,10 @@
 import sys
 
-file=open("treeTemplate1.txt");
+file=open("/usr/local/submitty/SubmittyAnalysisTools/treeTemplate1.txt");
 
 output=file.read();
 file=open(sys.argv[1]);
 output+="\n\t\t\tvar file="+file.read()+";\n";
-file=open("treeTemplate2.txt");
+file=open("/usr/local/submitty/SubmittyAnalysisTools/treeTemplate2.txt");
 output+=file.read();
 print(output);
