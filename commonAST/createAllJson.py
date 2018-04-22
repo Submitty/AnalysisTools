5f8f95dc12abbdb468f0fb51a85b9cfc633d4ea1
elizabethdinella <elizabeth.a.dinella@gmail.com>
2018-04-22 12:33:14 -0400

    made changes to looping directories

--- commonAST/createAllJson.py
+++ commonAST/createAllJson.py
@@ -1,39 +1,40 @@
 import subprocess
 import sys
 import glob
+import os
 
 
 lang = sys.argv[2]
 if not lang == "py" and not lang == "cpp":
-	print("Language not supported. Use 'py' or 'cpp'")
+	print("Language", lang, "not supported. Use 'py' or 'cpp'")
 
 
 #directoryPrefix = "/usr/local/submitty/GIT_CHECKOUT_AnalysisTools/commonAST/py-test-files/"
 directoryPrefix = sys.argv[1]
 directory = directoryPrefix + "*." + lang
 
-
-for fname in glob.glob(directory):
-	filename_full = fname 
-	print("=================")
-	print(filename_full)
-	print("=================")
-	end = filename_full.rfind(".")
+for root, dirs, files in os.walk(directoryPrefix, topdown=False):
+	for fname in files:
+		if not fname.endswith("." + lang): continue
+		filename_full = os.path.join(root, fname)
+		print("=================")
+		print(filename_full)
+		print("=================")
+		end = filename_full.rfind(".")
 	
-	filename_extStripped = filename_full[:end]	
-	start = filename_extStripped.rfind("/") + 1
-	filename_extPrefixStripped = filename_extStripped[start:]
-
-	if lang == "py":
-		out = subprocess.check_output(["/usr/local/submitty/SubmittyAnalysisTools/diagnostics", "-l", "python", filename_full])
-	elif lang == "cpp":
-		out = subprocess.check_output(["/usr/local/submitty/SubmittyAnalysisTools/unionToolRunner.py", "-json", fname])
-
-	fw = open(directoryPrefix + "out/" + filename_extPrefixStripped + "Union.txt", "w")
-	fw.write(out.decode('utf-8'))
-
-
-	out = subprocess.check_output(["/usr/local/submitty/SubmittyAnalysisTools/commonast.py", "-json", fname, "-" + lang ])
-	fw2 = open(directoryPrefix + "out/" + filename_extPrefixStripped + "Intersect.txt", "w")
-	fw2.write(out.decode('utf-8'))
-
+		filename_extStripped = filename_full[:end]	
+		start = filename_extStripped.rfind("/") + 1
+		filename_extPrefixStripped = filename_extStripped[start:]
+			
+		if lang == "py":
+			out = subprocess.check_output(["/usr/local/submitty/SubmittyAnalysisTools/diagnostics", "-l", "python", filename_full])
+		elif lang == "cpp":
+			out = subprocess.check_output(["/usr/local/submitty/SubmittyAnalysisTools/unionToolRunner.py", "-json", filename_full])
+		
+		fw = open(directoryPrefix + "/out/" + filename_extPrefixStripped + "Union.txt", "w")
+		fw.write(out.decode('utf-8'))
+		
+		
+		out = subprocess.check_output(["/usr/local/submitty/SubmittyAnalysisTools/commonast.py", "-json", filename_full, "-" + lang ])
+		fw2 = open(directoryPrefix + "/out/" + filename_extPrefixStripped + "Intersect.txt", "w")
+		fw2.write(out.decode('utf-8'))
